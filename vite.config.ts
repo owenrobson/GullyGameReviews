09232f3b58de2ebbff5df90e2b01406cf22286c6
Owen Robson <owen.robson13@hotmail.co.uk>
2025-06-10 21:53:01 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/gullygamereviews.io/',
   plugins: [react()],
   resolve: {
     alias: {
